7e6df8754e096e8c370016c74d5fdf61dfcdbfc3
Mario Rivera <ace.mrivera@gmail.com>
2025-06-08 21:54:31 -0600

    added tetimonial placeholder

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,6 +1,7 @@
 import { PortfolioSection, AboutSection, HeroSection, ResumeSection, SkillSection, StatsSection } from "../sections";
 import useScrollSpy from "../hooks/useScrollSpy";
 import ServicesSection from "../sections/ServicesSection";
+import TestimonialSection from "../sections/TestimonialSection";
 
 const MainContent = ({ onSectionChange }: { onSectionChange: (section: string) => void }) => {
     const { sectionRefs } = useScrollSpy(onSectionChange);
@@ -14,7 +15,7 @@ const MainContent = ({ onSectionChange }: { onSectionChange: (section: string) =
             <ResumeSection ref={(el) => { sectionRefs.current["resume"] = el }} id="resume" />
             <PortfolioSection ref={(el) => { sectionRefs.current["portfolio"] = el }} id="portfolio" />
             <ServicesSection  ref={(el) => { sectionRefs.current["services"] = el }} id="services" />
-            {/* <TestimonialSection ref={(el) => { sectionRefs.current["testimonial"] = el }} id="testimonial" /> */}
+            <TestimonialSection ref={(el) => { sectionRefs.current["testimonial"] = el }} id="testimonial" />
         </main>
     );
 };
