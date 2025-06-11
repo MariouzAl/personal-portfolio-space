f4ed35a87121f8b8d452845691bd991d1f2fd16e
Mario Rivera <ace.mrivera@gmail.com>
2025-06-10 22:11:54 -0600

    added feature/internationalization 	modified:   package-lock.json 	modified:   package.json 	new file:   public/locales/en/translation.json 	new file:   public/locales/es/translation.json 	new file:   src/@types/i18next.d.ts 	new file:   src/components/LanguageSelector.tsx 	modified:   src/components/NavMenu.tsx 	modified:   src/components/Navbar.tsx 	new file:   src/i18n.ts 	modified:   src/main.tsx 	modified:   src/sections/AboutSection.tsx 	modified:   src/sections/HeroSection.tsx 	modified:   src/services/AboutService.ts

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -1,17 +1,19 @@
 import { forwardRef } from "react";
 import useAboutInfo from "../hooks/useAboutInfo";
 import { Link } from "react-router"; // o "react-router" según tu versión
+import { useTranslation } from "react-i18next";
 
 
 
 
 const AboutSection = forwardRef<HTMLElement, { id: string }>((props, ref) => {
+    const { t } = useTranslation();
     const person = useAboutInfo();
     return (
         <section ref={ref} id={props.id} className="text-color-[var(--default-color)] bg-[var(--background-color)] py-16 px-3 overflow-clip">
             <div className="pb-16 relative w-full" data-aos="fade-up">
-                <h2 className="emphasis-title" >About</h2>
-                <p>{person?.description}</p>
+                <h2 className="emphasis-title" >{t('nav_menu.about')}</h2>
+                <p>{t('about.description')}</p>
             </div>
 
             <div className="@container">
@@ -21,9 +23,9 @@ const AboutSection = forwardRef<HTMLElement, { id: string }>((props, ref) => {
                         <img src="./img/my-profile-img.jpeg" className="size-full" alt="" />
                     </div>
                     <div className="mt-5 lg:flex-2/3">
-                        <h2 className="font-bold text-2xl font-raleway">Fullstack JS Developer / Watercolor artist / Padel trainer  </h2>
+                        <h2 className="font-bold text-2xl font-raleway">{t("about.subtitle")}</h2>
                         <p className="py-3 italic font-roboto font-[400]">
-                            I'm a fullstack software engineer with over 10 years of experience in building robust and scalable solutions using JavaScript and TypeScript. My expertise spans from developing interactive frontends with React and Angular to implementing efficient APIs with Node.js and Express, as well as managing SQL and NoSQL databases. Passionate about software architecture and system optimization, I'm dedicated to transforming complex concepts into efficient, high-performance digital products. My holistic vision, also cultivated through my role as a watercolor artist and padel coach, allows me to approach challenges with creativity and a solid strategy.
+                            {t("about.description2")}
                         </p>
                         <p className="py-3 italic font-roboto font-[400]">Here's some facts about me:</p>
                         <div className="flex flex-col  lg:flex-row ">
@@ -45,7 +47,7 @@ const AboutSection = forwardRef<HTMLElement, { id: string }>((props, ref) => {
                         <p className="py-3">
                             My professional journey has focused on developing complex and mission-critical systems, always with a strong foundation in object-oriented programming to ensure code maintainability and scalability. While my primary strength lies in development, my growing interest in software architecture drives me to design systems from a comprehensive perspective, considering scalability, security, and efficiency from conception. I possess knowledge of cloud services and am actively deepening my understanding of serverless architectures and cloud design patterns to lead larger-scale projects.
                         </p>
-{/*                         <p>
+                        {/*                         <p>
                             Outside of technology, my artistic and sports passions enrich my professional profile. As a watercolor artist, I cultivate my creativity, attention to detail, and the ability to visualize abstract solutions. Being a padel coach has endowed me with leadership, effective communication, and strategic thinking skills, which are fundamental for guiding technical teams and solving problems under pressure. These experiences allow me to approach engineering challenges with an innovative perspective and strong execution capability.
                         </p>
  */}
