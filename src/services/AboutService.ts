f12fa2125ec0cfc2b4a8c17e3f787f3a43f1366e
Mario Rivera <ace.mrivera@gmail.com>
2025-07-24 14:56:54 -0600

    updated python skill

--- src/services/AboutService.ts
+++ src/services/AboutService.ts
@@ -148,7 +148,7 @@ export class AboutService {
       new SkillInfo("UML", 70, 2009),
       new SkillInfo("Design Patterns", 75, 2018),
       new SkillInfo("OOP", 90, 2018),
-      new SkillInfo("Python", 60, new Date(2025, 0, 1)),
+      new SkillInfo("Python", 60, 2020),
       new SkillInfo("Android Development", 50, 2015),
     ];
   }
